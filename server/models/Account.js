f994c3a04d42397025bc3545667f774256b3c6c8
pio-kniaz <pio.kniaz@gmail.com>
2020-01-31 19:33:28 +0100

    Auth added , connected with db

---
+++ server/models/Account.js
@@ -0,0 +1,61 @@
+/* eslint-disable func-names */
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
+
+const { Schema } = mongoose;
+
+const accountSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  refreshToken: {
+    type: String,
+  },
+});
+
+accountSchema.statics.findByCredentials = async function (payload) {
+  const { userName, password } = payload;
+  const account = this;
+  const accountData = await account.findOne({ userName });
+
+  if (!account) {
+    throw new Error('User not found');
+  }
+  const isMatch = await bcrypt.compare(password, accountData.password);
+
+  if (!isMatch) {
+    throw new Error('Password not correct');
+  }
+  return accountData;
+};
+
+accountSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({
+    id: user.id,
+  }, keys.accessToken, { expiresIn: '15s' });
+
+  return token;
+};
+
+accountSchema.methods.generateRefreshToken = async function () {
+  const user = this;
+  const token = jwt.sign({
+    id: user.id,
+  }, keys.refreshToken, { expiresIn: '7d' });
+
+  return token;
+};
+
+const Account = mongoose.model('Account', accountSchema);
+
+module.exports = Account;
