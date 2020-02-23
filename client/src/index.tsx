39f9774ed08086e29c6aac8f6f02a872d67501d2
Piotr Kniaź <piotrkniaz@Piotrs-MacBook-Pro.local>
2020-02-23 19:45:11 +0100

    style fixed

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
 import * as serviceWorker from 'serviceWorker';
-import 'style/main.scss';
+import 'style/index.scss';
+import 'style/global.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
