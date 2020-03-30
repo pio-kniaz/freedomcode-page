be483359c2745fd2c0900459278f034563ab006d
Piotr Kniaź <pio.kniaz@gmail.com>
2020-03-30 23:25:40 +0200

    validation fixed

--- client/src/components/contact/contact-form/ContactForm.tsx
+++ client/src/components/contact/contact-form/ContactForm.tsx
@@ -33,7 +33,7 @@ const initialValues: IFormValues = {
 };
 
 const contactSchema = Yup.object().shape({
-  name: Yup.string().min(2).max(30).required(),
+  name: Yup.string().min(3).max(30).required(),
   subject: Yup.string().min(3).max(30).required(),
   email: Yup.string().email().required(),
   message: Yup.string().min(3).max(500).required(),
